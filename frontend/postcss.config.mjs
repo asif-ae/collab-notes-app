7220c8694298a865e59e5320b2ec89bbef8d76f0
Asif Estiak <me.asif.ae@gmail.com>
2025-03-15 16:30:06 +0600

    feat: initialized and cleanup frontend project

---
+++ frontend/postcss.config.mjs
@@ -0,0 +1,13 @@
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "monospace"],
+      },
+    },
+  },
+};
+
+export default config;
